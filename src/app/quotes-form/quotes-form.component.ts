ebb77f34457ee25d6c2e2edff413cbc352fd8013
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-08-21 12:09:19 +0300

    adding eventemmiter to the for component

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter  } from '@angular/core';
 import { Quotes } from '../quote';
 import { Input } from '@angular/core';
 
@@ -9,7 +9,14 @@ import { Input } from '@angular/core';
 })
 export class QuotesFormComponent implements OnInit {
 newQuote= new Quotes(0,"","","",new Date());
-@Input () quotesarr:Quotes[]=[{id:1,quotedetails:"ttttt",authour:"sssss",submitby:"yyyyy",addeddate: new Date(2020,12,12)}];
+ quotesarr:Quotes[]=[{id:1,quotedetails:"ttttt",authour:"sssss",submitby:"yyyyy",addeddate: new Date(2020,12,12)}];
+
+ @Output() addQuote=new EventEmitter<Quotes>();
+
+ submitQuote(){
+   this.addQuote.emit(this.newQuote);
+ }
+
   constructor() { }
 
   ngOnInit(): void {
