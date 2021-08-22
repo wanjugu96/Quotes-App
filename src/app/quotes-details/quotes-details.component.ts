01b20e179ec7766d1264bda9cfc04b9d5459ba39
wanjugu mung'au <wanjugu.mung'au@strathmore.edu>
2021-08-22 14:06:36 +0300

    ading the down and upvote functionality

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -13,22 +13,22 @@ export class QuotesDetailsComponent implements OnInit {
   @Input()
   quotess!: Quotes;
   //quotes=quotes
-//   @Output() isComplete=new EventEmitter<boolean>();
+ @Output() isComplete=new EventEmitter<boolean>();
   
-// downvotes:number=0;
-// upvotes:number=0;
+downvotes:number=0;
+upvotes:number=0;
 
-//   upvote(){
+  upvote(){
 
-// this.upvotes=this.upvotes+1;
-// return this.upvotes;
-//   }
+this.upvotes=this.upvotes+1;
+return this.upvotes;
+  }
 
-// downvote(){
-//   this.downvotes=this.downvotes+1;
-//   return this.downvotes
-// }
-// downvotess:number=this.downvotes
+downvote(){
+  this.downvotes=this.downvotes+1;
+  return this.downvotes
+}
+downvotess:number=this.downvotes
 // deleteQuote(complete:boolean){
 //   this.isComplete.emit(complete);
 // }
